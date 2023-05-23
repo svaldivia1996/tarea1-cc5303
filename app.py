c776a3ee6cb54055032ee0a81bd96237ac9aeb9f
svaldivia1996 <sebastianvaldiviareyes@hotmail.com>
2023-05-23 11:45:34 -0400

    fix: new approach

--- app.py
+++ app.py
@@ -5,12 +5,10 @@ app = Flask(__name__)
 
 # Lista para almacenar los nodos conocidos
 known_nodes = []
-db = redis.Redis(host='redis', port=6379)  # Conexión a la base de datos Redis
 
 
 @app.route('/nodes', methods=['GET'])
 def get_nodes():
-    known_nodes = db.lrange('known_nodes', 0, -1)
     return jsonify(known_nodes)
 
 
@@ -19,7 +17,6 @@ def add_node():
     node = request.get_json()
     if 'address' in node and 'port' in node:
         known_nodes.append(f"http://{node['address']}:{node['port']}")
-        db.rpush('known_nodes', node_string)  # Agregar el nodo a la lista en Redis
         announce_node_to_known_nodes(node['address'], node['port'])
         return jsonify({'message': 'Node added successfully'})
     else:
@@ -37,4 +34,6 @@ def announce_node_to_known_nodes(address, port):
             pass
 
 if __name__ == '__main__':
+    self_node_url = 'http://localhost:8083'
+    known_nodes.append(self_node_url)
     app.run(host='0.0.0.0', port=8083)
